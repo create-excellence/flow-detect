b04c8a48c35f1a73b052a81b18deecd9f51bc405
allworldg <944937881@qq.com>
2021-02-28 16:21:19 +0800

    chore: 添加获取摄像头最新流量接口

---
+++ detect-service/detect-flow/src/main/java/com/explore/controller/CameraController.java
@@ -0,0 +1,58 @@
+package com.explore.controller;
+
+import com.explore.client.CameraClient;
+import com.explore.common.ServerResponse;
+import com.explore.common.database.Camera;
+import com.explore.utils.HttpUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author PinTeh
+ * @date 2020/7/4
+ */
+@RestController
+@RequestMapping("/api/v1/camera")
+public class CameraController {
+
+    private final String rtspPath = "rtsp://%s:%s@%s:%s/h264/ch1/main/av_stream";
+    private final String pushUrl = "rtsp://%s:%s@%s:%s/h264/ch1/main/av_stream";
+
+    private final String path = "http://localhost:9090";
+
+    private final CameraClient cameraClient;
+
+    public CameraController(CameraClient cameraClient) {
+        this.cameraClient = cameraClient;
+    }
+
+    /**
+     * 推流摄像头
+     */
+    @GetMapping("/start")
+    public ServerResponse start(Integer cid){
+        ServerResponse<Camera> serverResponse = cameraClient.getById(cid);
+        Camera camera = serverResponse.getData();
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("cid", String.valueOf(cid));
+        params.add("source", camera.getSource());
+        params.add("pushUrl", camera.getPushUrl());
+        String startUrl = path + "/camera/start";
+        return HttpUtils.post(startUrl, params);
+    }
+
+    /**
+     * 断流摄像头
+     */
+    @GetMapping("/close")
+    public ServerResponse close(Integer cid){
+        // TODO : check
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("cid", String.valueOf(cid));
+        String stopUrl = path + "/camera/stop";
+        return HttpUtils.post(stopUrl,params);
+    }
+}
--- detect-service/detect-flow/src/main/java/com/explore/controller/FlowController.java
+++ detect-service/detect-flow/src/main/java/com/explore/controller/FlowController.java
@@ -2,6 +2,8 @@ package com.explore.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.explore.client.CameraClient;
+import com.explore.client.UserClient;
 import com.explore.common.ServerResponse;
 import com.explore.common.database.Flow;
 import com.explore.servcie.IFlowService;
@@ -9,6 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author PinTeh
@@ -21,9 +27,15 @@ public class FlowController {
 
     private final IFlowService flowService;
 
+    private final UserClient userClient;
 
-    public FlowController(IFlowService flowService) {
+    private final CameraClient cameraClient;
+
+
+    public FlowController(IFlowService flowService,UserClient userClient,CameraClient cameraClient) {
         this.flowService = flowService;
+        this.userClient = userClient;
+        this.cameraClient = cameraClient;
     }
     /**
      * 保存检测数据
@@ -44,6 +56,22 @@ public class FlowController {
     public ServerResponse<Flow> get(@RequestParam("cid") Integer cid) {
         Flow flow = flowService.getOne(new QueryWrapper<Flow>().eq("camera_id", cid).orderByDesc("id").last("limit 0,1"));
         return ServerResponse.createBySuccess(flow);
+    }
+
+    /**
+     * 获取最新集合记录
+     *
+     * @return ServerResponse
+     */
+    @GetMapping("/ids")
+    public ServerResponse getList(@RequestParam("ids") String ids) {
+        String[] strings =ids.split(",");
+        List<Flow> res = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++) {
+            Flow flow = flowService.getOne(new QueryWrapper<Flow>().eq("camera_id", strings[i]).orderByDesc("id").last("limit 0,1"));
+            res.add(flow);
+        }
+        return ServerResponse.createBySuccess(res);
     }
 
     /**

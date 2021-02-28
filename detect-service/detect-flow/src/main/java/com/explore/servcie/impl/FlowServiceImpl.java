baf5514472e3c36bcf294ea64ce54351bd06f018
PinTeh <794409767@qq.com>
2021-02-28 12:31:01 +0800

    chore: 修改超时时间、修改快照实现

--- detect-service/detect-flow/src/main/java/com/explore/servcie/impl/FlowServiceImpl.java
+++ detect-service/detect-flow/src/main/java/com/explore/servcie/impl/FlowServiceImpl.java
@@ -16,17 +16,19 @@ import com.explore.servcie.IFlowService;
 import com.explore.servcie.IWarningService;
 import com.explore.socket.WebMessage;
 import com.explore.socket.WebSocketServer;
-import com.explore.utils.UploadUtil;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.text.DateFormat;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -39,6 +41,9 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Service
 public class FlowServiceImpl extends ServiceImpl<FlowMapper, Flow> implements IFlowService {
+
+    @Value("${detect.snapshot.upload-path}")
+    private String snapshotUploadPath;
 
     private static Cache<String, String> cache;
     private static Cache<String, String> cameraCache;
@@ -99,44 +104,39 @@ public class FlowServiceImpl extends ServiceImpl<FlowMapper, Flow> implements IF
 
         String token = WebSocketServer.tokenMAP.get(camera.getUserId().toString());
         if(token!=null){
-
             webSocketServer.sendMessageToAll(token
                     , WebMessage.createFlowMessage(String.valueOf(flow.getFlow()),LocalDateTime.now(),camera.getId()).toJson());
         }
-        String key = UploadUtil.check(file);
-        boolean isSave = false;
+
+        // 保存图片到服务器
+        String fileName = saveSnapshotsFile(file, camera.getId());
+        String filePath = "/" + camera.getId() + "/" + fileName;
+
         if (flow.getFlow().compareTo(camera.getWarning().longValue()) > 0) {
             String value = cache.getIfPresent(String.valueOf(camera.getId()));
             if (value != null) {
                 return;
             }
-            if(key!=null){
-                UploadUtil.uploadQiniu(file,key);
-                isSave =true;
-                Snapshot snapshot =new Snapshot();
-                snapshot.setCameraId(Long.valueOf(camera.getId()));
-                snapshot.setInfo("人流量超出预警值");
-                snapshot.setPath(key);
-                snapshot.setStatus(1);//1代表预警
-                snapshotClient.add(snapshot);
-            }
+            Snapshot snapshot =new Snapshot();
+            snapshot.setCameraId(Long.valueOf(camera.getId()));
+            snapshot.setInfo("人流量超出预警值");
+            snapshot.setFileName(fileName);
+            snapshot.setPath(filePath);
+            // 1代表预警
+            snapshot.setStatus(1);
+            snapshotClient.add(snapshot);
             LocalDateTime now = LocalDateTime.now();
             cache.put(String.valueOf(camera.getId()), now.toString());
             warningService.save(Warning.builder().number(flow.getFlow().intValue()).warning(camera.getWarning()).createTime(now).build());
             log.info(" 异常人流量 : warning => flow value : {}", flow.getFlow());
         }
-        boolean isCover =((int) (Math.random() * 10))%100==0;
+        boolean isCover =new Random().nextInt(10) % 100 == 0;
         if(isCover){
-            if(key!=null&&!isSave){
-                UploadUtil.uploadQiniu(file,key);
-            }
-            camera.setCover(key);
+            camera.setCover(filePath);
             cameraClient.reviseCamera(camera,camera.getId());
         }
 
     }
-
-
 
     @Override
     public ServerResponse getUserCameraAndFlow() {
@@ -162,5 +162,32 @@ public class FlowServiceImpl extends ServiceImpl<FlowMapper, Flow> implements IF
 
         }
         return null;
+    }
+
+    private String saveSnapshotsFile(MultipartFile file,Integer cameraId){
+        String originalFilename = file.getOriginalFilename();
+        if (StringUtils.isEmpty(originalFilename)){
+            log.error("get file original filename is empty");
+            return null;
+        }
+
+        String ext = originalFilename.substring(originalFilename.indexOf("."));
+        String timeFileNamePrefix = new SimpleDateFormat("yyyyMMdd-HHmmss-SSS").format(new Date());
+        String newFileName = timeFileNamePrefix + ext;
+
+        String dir = snapshotUploadPath + "/" + cameraId + "/";
+        File fileDir = new File(dir);
+        if (!fileDir.exists()){
+            boolean mkdirs = fileDir.mkdirs();
+            log.info("create file dir : result = {}.", mkdirs);
+        }
+        File localFile = new File(dir + newFileName);
+        try {
+            file.transferTo(localFile);
+        } catch (IOException e) {
+            log.error("save file error",e);
+        }
+
+        return newFileName;
     }
 }
